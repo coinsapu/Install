53e734477f7ff10df86c4dd5ae53b31673616e47
coinsapu <coinsapu@gmail.com>
2018-05-18 22:24:51 +1000

    new

--- Sparks_Install.sh
+++ Sparks_Install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-set -e
-
-fi
-
 sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get install -y nano htop git && sudo apt-get install -y software-properties-common && sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev && sudo apt-get install -y libboost-all-dev && sudo apt-get install -y libevent-dev && sudo apt-get install -y libminiupnpc-dev && sudo apt-get install -y autoconf && sudo apt-get install -y automake unzip && sudo add-apt-repository  -y  ppa:bitcoin/bitcoin && sudo apt-get update && sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
 echo "################################################"
